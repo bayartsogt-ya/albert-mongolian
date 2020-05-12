f2d852b09fdb076d205f601a269c8546cb60d370
Bayartsogt Yadamsuren <bayartsogtyadamsuren@icloud.com>
2020-05-13 07:22:09 +0800

    pretraining data

--- build_pretraining_data.sh
+++ build_pretraining_data.sh
@@ -1,15 +1,28 @@
-python3 -m albert.create_pretraining_data \
-      --input_file=./mn_corpus/all.txt \
-      --output_file=./mn_corpus/tf_records/all-maxseq-${max_seq_length}.tfrecord \
-      --spm_model_file=./sp_models/30k-clean-mn.model \
-      --vocab_file=./sp_models/30k-clean-mn.vocab \
-      --do_lower_case=False \
-      --max_seq_length=512 \
-      --max_predictions_per_seq=20 \
-      --masked_lm_prob=0.15 \
-      --random_seed=12345 \
-      --dupe_factor=5 \
-      --do_whole_word_mask=False \
-      --do_permutation=False \
-      --favor_shorter_ngram=False \
-      --random_next_sentence=False
+basedir=$1
+max_seq_len=$2
+
+for path in $basedir/*.txt
+    do
+        file_name=${path##*/}
+        new_name="$(cut -d'.' -f1 <<<"$file_name")"-maxseq-${max_seq_len}.tf_record
+        # echo ${path}
+        # echo ${file_name}
+        echo from ${basedir}/${file_name}  --  to ${basedir}/${new_name}
+
+        python3 -m albert.create_pretraining_data \
+            --input_file=${basedir}/${file_name} \
+            --output_file=${basedir}/${new_name} \
+            --spm_model_file=./sp_models/30k-clean-mn.model \
+            --vocab_file=./sp_models/30k-clean-mn.vocab \
+            --do_lower_case=False \
+            --max_seq_length=${max_seq_len}\
+            --max_predictions_per_seq=20 \
+            --masked_lm_prob=0.15 \
+            --random_seed=12345 \
+            --dupe_factor=5 \
+            --do_whole_word_mask=False \
+            --do_permutation=False \
+            --favor_shorter_ngram=False \
+            --random_next_sentence=False
+
+    done
